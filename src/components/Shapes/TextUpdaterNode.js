a3e02d65748e558c4b514419c688f1afde71cbd3
abhishekkumar008 <abhishek.kumar@hsv.digital>
2022-11-17 00:16:39 +0530

    added changes for react flow shapes

---
+++ src/components/Shapes/TextUpdaterNode.js
@@ -0,0 +1,24 @@
+import { useCallback } from 'react';
+import { Handle, Position } from 'reactflow';
+
+const handleStyle = { left: 10 };
+
+function Circle({ data }) {
+  const onChange = useCallback((evt) => {
+    console.log(evt.target.value);
+  }, []);
+
+  return (
+    <div className="circle">
+      <Handle type="target" position={Position.Top} />
+      <div>
+        {/* <label htmlFor="text">Text:</label> */}
+        {/* <input id="text" name="text" onChange={onChange} /> */}
+      </div>
+      {/* <Handle type="source" position={Position.Bottom} id="a" style={handleStyle} /> */}
+      <Handle type="source" position={Position.Bottom} id="b" />
+    </div>
+  );
+}
+
+export default Circle;
---
+++ src/components/Shapes/index.js
@@ -0,0 +1,57 @@
+import { useCallback, useState } from 'react';
+import ReactFlow, { addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow';
+import 'reactflow/dist/style.css';
+
+import Circle from './TextUpdaterNode.js';
+import Triangle from './TringleNode.js';
+import Trapezoid from './Trapezoid.js';
+import Rectangle from './Rectangle.js';
+
+import './text-updater-node.css';
+
+const rfStyle = {
+  backgroundColor: '#B8CEFF',
+};
+
+const initialNodes = [
+  { id: 'node-1', type: 'circle', position: { x: 400, y: 0 }, data: { value: 123 } },
+  { id: 'node-2', type: 'triangle', position: { x: 10, y: 10 }, data: { value: 123 } },
+  { id: 'node-3', type: 'trapezoid', position: { x: 20, y: 20 }, data: { value: 123 } },
+  { id: 'node-4', type: 'rectangle', position: { x: 30, y: 30 }, data: { value: 123 } },
+];
+// we define the nodeTypes outside of the component to prevent re-renderings
+// you could also use useMemo inside the component
+const nodeTypes = { circle: Circle, triangle: Triangle, trapezoid: Trapezoid, rectangle: Rectangle };
+
+function ShapeFlow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState([]);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      nodeTypes={nodeTypes}
+      fitView
+      style={rfStyle}
+    />
+  );
+}
+
+export default ShapeFlow;
